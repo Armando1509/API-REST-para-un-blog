a368a1b11eef337103e27500992981bfeebcbe19
Armando1509 <armandogomez1509@gmail.com>
2024-10-16 13:07:13 -0600

    creacion de get pruebas

--- index.js
+++ index.js
@@ -1,9 +1,64 @@
 const { conexion } = require("./basedatos/conexion")
+const express = require("express");
+const cors = require("cors")
 
 //Inicializar app
 
 console.log("App de node arrancada");
 
-// Conexion
+// Conectar a la Base de Datos
 
 conexion();
+
+// Crear el servidor Node
+
+const app = express();
+const puerto = 3900
+
+// Configurar cors
+
+app.use(cors())
+
+// Convertir body a objeto js
+
+app.use(express.json());
+
+// Crear rutas
+
+app.get("/probando", (req, res) =>{
+
+    /* return res.status(200).send(`
+            <div>
+                <h1>Probando ruta nodejs</h1>
+                <p>Creando api rest con node</p>
+            </div>
+        `) */
+    return res.status(200).json([{
+        nombre: "conde",
+        edad: 35,
+        nacionalidad: "mexicana"
+    },
+    {
+        nombre: "Armando",
+        edad: 15,
+        nacionalidad: "quien sabe"
+    }
+])
+    
+})
+
+app.get("/", (req, res) =>{
+    return res.status(200).send(`
+        <div>
+            <h1>Probando ruta nodejs</h1>
+            <p>Creando api rest con node</p>
+        </div>
+    `) 
+})
+
+// Crear servidor y escuchar peticiones http
+
+app.listen(puerto, ()=>{
+    console.log("Servidor corriendo en el puerto "+puerto);
+    
+})
